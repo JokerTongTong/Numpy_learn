2acac0abce9a2382f2fca4dade5977f1f0fc4fa7
JokerTongTong <guoqiang19951212@163.com>
2019-06-18 09:17:06 +0800

    2.12将NumPy数组转换为Python列表

--- 2.12将NumPy数组转换为Python列表.py
+++ 2.12将NumPy数组转换为Python列表.py
@@ -3,7 +3,13 @@ import numpy as np
 '''
 NumPy数组转换为Python列表
 方法1：tolist
+    将NumPy数组转换为Python列表
+    一维数组 -> 列表
+    二维数组 -> 嵌套列表
 方法2：astype
+    a.astype(int/str/bool/...)
+    将数组a所有元素转换为对应类型(int/str/bool/...)
+    返回一个新数组,a不会被修改
 '''
 
 # 方法1：tolist
@@ -17,3 +23,5 @@ d = c.tolist()
 print("Python列表d：{}".format(d))
 
 # 方法2：astype
+print(a.astype(str))
+print(a)

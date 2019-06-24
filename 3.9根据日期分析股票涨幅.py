b2b119a0991bcb7421ba072b5f4c21d224223b81
JokerTongTong <guoqiang19951212@163.com>
2019-06-24 17:35:37 +0800

    3.9根据日期分析股票涨幅

--- 3.9根据日期分析股票涨幅.py
+++ 3.9根据日期分析股票涨幅.py
@@ -1,5 +1,6 @@
 # 3.9根据日期分析股票涨幅
 import numpy as np
+from datetime import datetime
 '''
 根据日期分析股票涨幅
 涉及函数:
@@ -8,20 +9,23 @@ import numpy as np
     3)argmin(a):取数组中最小值对应的索引
 '''
 
-# 1)简单收益率
-a = np.array([20, 30, 50, 45])
-b = np.diff(a) / a[:-1]
 
-# 2)对数收益率
-c = np.diff(np.log(a))
-d = np.sqrt(a)
+def datestr2num(time):
+    return datetime.strptime(time.decode('utf-8'),'%d-%m-%Y').date().weekday()
 
-# 过滤正数收益率
-e = np.where(a)
 
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(e[0])
+data,b = np.loadtxt('data.csv',delimiter=',',usecols=(1,6),unpack=True
+                 ,converters={1:datestr2num})
+
+# data数组进行强制类型转换,元组中的每个元素都转换为int类型
+data = data.astype(int)
+
+
+'''
+'data.csv' -> filebath
+delimiter -> 分割符
+usecols=(1,6) -> 要操作的列的索引,元组的形式,可以多列(0,1,2,3,4,5)
+unpack=True -> 拆包 默认False
+converters={1:datestr2num} -> 参数为字典,key是要转换的列的【索引】,
+                                       value是转换用的函数名
+'''
